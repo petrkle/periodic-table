838d86d6ea3a05b54633e1bcf9b75a6ed71531aa
Petr <petr@kle.cz>
2014-08-25 21:35:46 +0200

    Page 404 removed from sitemap

--- sitemap.sh
+++ sitemap.sh
@@ -8,7 +8,7 @@ echo -n '<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ' > $SITEMAP
 
-for url in `find $WWW/ -type f -name "*.html" -printf "%P\n"`
+for url in `find $WWW/ -type f -name "*.html" -not -name "404.html" -printf "%P\n"`
 do
 	echo "<url><loc>$HOME/$url</loc><changefreq>monthly</changefreq></url>" >> $SITEMAP
 done
