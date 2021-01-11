1825256d7c2a0705ef292fa2b7756b8b42a3583a
Petr <petr@kle.cz>
2021-01-11 15:54:26 +0100

    Gziped files for nginx

--- upload.sh
+++ upload.sh
@@ -2,11 +2,14 @@
 
 find www -name "*.html" -exec sed -i "s/pt.kl.cz/pt.kle.cz/g" \{\} \;
 
+for foo in `find www -type f -not -name "*.gz"`; do gzip -c --best $foo > $foo.gz; done
+
 rsync -e ssh \
 	--recursive \
 	--stats \
 	--verbose \
 	--times \
+	--delete-after \
 	--update \
 	www/ \
 	vps.kle.cz:/home/www/pt.kle.cz/
