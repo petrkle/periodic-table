a0bf1d5544f1cc34ae942bf5480a996f3668db94
Petr <petr@kle.cz>
2025-08-13 22:47:20 +0200

    brotli

--- upload.sh
+++ upload.sh
@@ -4,6 +4,8 @@ find www -name "*.html" -exec sed -i "s/pt.kl.cz/pt.kle.cz/g" \{\} \;
 
 for foo in `find www -type f -not -name "*.gz"`; do gzip -c --best $foo > $foo.gz; done
 
+for foo in `find www -type f -not -name "*.gz"`; do brotli -c -Z $foo > $foo.br; echo -n "."; done
+
 rsync -e ssh \
 	--recursive \
 	--stats \
